a60d94b3610fcea4f930bc66924c9a1ef3437d70
David Smith <davids3987@gmail.com>
2019-10-08 10:03:00 -0600

    second function

--- list_quiz.rb
+++ list_quiz.rb
@@ -13,4 +13,23 @@ end
 
 puts three_even([1, 4, 6, 4])
 puts three_even([2, 1, 3, 5])
-puts three_even([2, 1, 6, 4])
+
+
+def biggertwo(list, list1)
+    total = 0
+    total1 = 0
+    list.each do |n|
+        total = total + n
+        total1 = total1 + n
+    end
+    if total >= total1 
+        return list
+    else 
+        return list1
+    end
+end
+
+print biggertwo([3, 6], [1, 2])
+print biggertwo([1, 9], [3, 4])
+print biggertwo([3, 3], [5, 1])
+    
